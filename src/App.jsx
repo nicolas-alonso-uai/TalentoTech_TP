b07c17f56f3b5aae69511b31f1c79a9acb8c2483
MrCrowly_UAI <nicolas.alonso.uai@gmail.com>
2025-12-14 15:15:19 -0300

    [FEAT]Formulario de alta y admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { CartProvider } from "./context/CartContext/CartProvider";
 import { Cart } from "./components/Cart/Cart";
 import { ProductFormContainer } from "./components/adminComponents/ProductFormContainer/ProductFormContainer";
+import { MainLayout } from "./layouts/MainLayout";
+import { AdminLayout } from "./layouts/AdminLayout";
+import { RutaProtegida } from "./components/RutaProtegida/RutaProtegida";
+import { Login } from "./components/Login/Login"; 
 
 function App() {
   return (
@@ -15,19 +18,34 @@ function App() {
       <BrowserRouter>
         <CartProvider>
           <div>
-            <Header />
             <Routes>
-              <Route
-                path="/"
-                element={<ItemListContainer titulo={"Bienvenidos a Mr.Tools"} />}
-              />
-              <Route
-                path="/category/:category"
-                element={<ItemListContainer titulo={"Bienvenidos a Mr.Tools"} />}
-              />
-              <Route path="/detail/:id" element={<ItemDetailContainer />} />
-              <Route path="/carrito" element={<Cart />} />
-              <Route path="/admin" element={<ProductFormContainer />} />
+              <Route element={<MainLayout/>}>
+                <Route
+                  path="/"
+                  element={
+                    <ItemListContainer titulo={"Bienvenidos a Mr.Tools"} />
+                  }
+                />
+                <Route
+                  path="/category/:category"
+                  element={
+                    <ItemListContainer titulo={"Bienvenidos a Mr.Tools"} />
+                  }
+                />
+                <Route path="/detail/:id" element={<ItemDetailContainer />} />
+                <Route path="/carrito" element={<Cart />} />
+              </Route>
+              <Route path="/admin" element={<AdminLayout/>}>
+                <Route index element={<Login />} />
+                <Route
+                  path="alta-productos"
+                  element={
+                    <RutaProtegida>
+                      <ProductFormContainer />
+                    </RutaProtegida>
+                  }
+                />
+              </Route>
             </Routes>
             <Footer />
           </div>
