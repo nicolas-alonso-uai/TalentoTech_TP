b086fbf679d580eb9566ae40b7cd9ddcd1e9d197
MrCrowly_UAI <nicolas.alonso.uai@gmail.com>
2025-12-14 15:38:33 -0300

    [FEAT] FINAL - Funcionalidad con la API para la carga

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext/authProvider.jsx'
 
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
